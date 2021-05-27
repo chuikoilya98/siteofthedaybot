11b0447796a806a93875722f542865229142aab5
chuikoilya98 <49401081+chuikoilya98@users.noreply.github.com>
2021-05-27 13:13:20 +0500

    сделал основу бота, получилось запустить очередь с runOnce, надо доделать с сутками

--- bot.py
+++ bot.py
@@ -2,7 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import random
 import sqlite3
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
+#TODO: Перенести в разные файлы
 class Database() :
     pass
 
@@ -75,3 +79,41 @@ class Inspiration() :
 
         return text
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+def start(update: Update, _: CallbackContext) -> None :
+    #make db write for info about User
+    update.message.reply_text('Привет, я бот, который умеет искать и предлагать необычные сайты. Чтобы подписаться на ежедневную рассылку, нажми /siteoftheday')
+
+def job(context: CallbackContext) -> None :
+    
+    job = context.job
+    aww = Inspiration()
+    text = aww.getSiteOfTheDay()
+    context.bot.send_message(job.context, text = text)
+
+def sendDailyMessage(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    context.job_queue.run_daily(callback = job,time = None, context= chat_id, name= str(chat_id))
+
+def main() -> None:
+
+    updater = Updater("1755982457:AAGzuubLxQMJ36h8wjDsgTjvHZgUW6wiyRE")
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("siteoftheday", sendDailyMessage))
+
+    # Start the Bot
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
